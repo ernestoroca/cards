a65932845a4c779c1fb874d14a7fba152315c75e
ernestoroca <ernesto.roca@gmail.com>
2021-05-27 18:53:20 -0400

    Update textos.js

--- textos.js
+++ textos.js
@@ -3,8 +3,8 @@ function getTexto(num){
     return `
 <p><b>El cetro de los elementos</b></p>
 <p>Es la llama que aún no se ha encendido, la luz de la mañana que trae en sí todas las esperanzas de un día que no ha pasado del todo, en el que todo puede suceder. Es el comienzo, el poder antes de la acción, el mago jugando con los elementos, el niño trayendo a sus ojos los sueños de miles de existencias posibles. En el cetro están todas las premisas para una vida que se va a actualizar: tiene en sí el gozo de la ilusión, que fácilmente puede convertirse en ilusión de gozo.</p>
-<p>Sí para los que aman el momento del comienzo, el que precede a la acción, cuando nada es concreto y reina el entusiasmo por la idea.</p>
-<p>No para los que se preocupan por las dificultades de un camino que no han seguido y temen carecer de potencialidades para superarlas.</p>
+<p><b>Sí para</b> los que aman el momento del comienzo, el que precede a la acción, cuando nada es concreto y reina el entusiasmo por la idea.</p>
+<p><b>No para</b> los que se preocupan por las dificultades de un camino que no han seguido y temen carecer de potencialidades para superarlas.</p>
 <p><b>Cuento e imperativo:</b><br>
 Le dice a alguien que se convertirá en el mejor piloto del mundo; le dice a alguien más que será el rey de los piratas; a otros les dice que se convertirá en un genio de la música. Está satisfecho con sus propósitos cambiantes. Porque todas son ciertas y nadie puede contradecirlo. Porque es un niño, con los sueños de mil existencias posibles aún en sus ojos.
 </p>
@@ -15,23 +15,54 @@ Le dice a alguien que se convertirá en el mejor piloto del mundo; le dice a alg
     return `
 <p><b></b></p>
 <p>Ícaro se eleva al cielo, sobre sus alas de plumas y cera vuela hacia el cálido sol, que derrite su arnés y lo hace caer al suelo. Hay una regla de causa y efecto según la cual cada uno recibe lo que se merece, y todo lo que sube tarde o temprano debe bajar, lo que aumenta debe disminuir al final, lo que vive está destinado a morir. Siempre hay columnas y anillos por los que tenemos que pasar: una lógica consecuencial que traza una línea entre lo que viene primero y lo que sigue. Y en esta cuerda, como equilibrista, el presente encuentra su equilibrio.</p>
-<p>Sí para aquellos que saben dar lo mejor de sí mismos cuando son conscientes de estar en un carril ya definido y conocen de antemano la evolución de aquello a lo que se dedican.</p>
-<p>No para aquellos que son torturados por tal sentimiento y quisieran rebelarse</p>
+<p><b>Sí para</b> aquellos que saben dar lo mejor de sí mismos cuando son conscientes de estar en un carril ya definido y conocen de antemano la evolución de aquello a lo que se dedican.</p>
+<p><b>No para</b> aquellos que son torturados por tal sentimiento y quisieran rebelarse</p>
 <p><b>Cuento e imperativo:</b><br>
 Extiende sus alas hechas de plumas y pegamento y se eleva en el cielo. Se eleva, trepa nubes y corrientes de aire, hasta que se aleja del sol a una pulgada. Allí, el pegamento se disuelve, las alas se desprenden. Pero sonríe con calma porque sabe que ha llegado a la cima. Y todo lo que sube, tarde o temprano, debe bajar.
 </p>
 <p><b>El correcto es siempre el más simple</b></p>
       `;
   }
-  if(num === 63){//8
+  if(num === 59){//21
     return `
-<p><b>La caída</b></p>
-<p></p>
-<p>Sí para </p>
-<p>No para </p>
+<p><b>Retorno eterno</b></p>
+<p>Es Unidad, Plenitud, Completitud, el Todo que baila y canta y consagra la alegría de vivir. Es Zorba quien juega sirtaki y llega a la eternidad, es el inmortal final feliz de los cuentos de hadas, es la entrada al paraíso de Dante. Estamos ante un éxito total que no se limita al plano material o espiritual, sino que proviene de la fusión de ambos. Todo está vivo y Dios está en todo, entonces todo es Dios. Todo está conectado con el otro en un esquema de necesidad natural, por lo que todo adquiere un sentido. Es el silogismo de la felicidad: la alegría por la alegría, que vuelve a la alegría.</p>
+<p><b>Sí para</b> aquellos que necesitan sentir que todo está en su lugar, que todos los puntitos están alineados y nada escapa al plan de una gran armonía.</p>
+<p><b>No para</b> los que consideran apasionante la acción y aburrida la conclusión, para los que, habiendo llegado al final de una obra, piensan: ¿Y ahora?</p>
 <p><b>Cuento e imperativo:</b><br>
+Camina y baila, se entrega a la alegría de la vida, pone un paso en cruz en su caminar, y da vueltas sobre sí misma así, sin razón, solo por el gusto de ver los colores del mundo girando a su alrededor. Y para que el mundo también se dé cuenta de que ella gira dentro de él.
 </p>
-<p><b></b></p>
+<p><b>Es hora de unir los puntos</b></p>
+      `;
+  }
+  if(num === 64){//10
+    return `
+<p><b>La totalidad</b></p>
+<p>La ley universal del tiempo está escrita aquí. Es la superación de la filosofía de Heráclito del «panta rei»: el agua no solo fluye, sino que también lo hace de forma cíclica, según un esquema preestablecido. Entonces, en las estaciones del mundo, sol y luna, se alternan eventos predecibles e impredecibles. El eterno retorno nos enseña que las cosas van y vienen, pero no tenemos que preocuparnos por ellas, no tenemos que vivir en el pasado ni en el futuro: cuando finalmente vemos y aceptamos el modelo de la rueda giratoria, Realmente podemos estar en el presente, disfrutarlo plenamente para mejor, soportarlo más irreflexivamente para peor, ya que sabemos que pronto desaparecerá.</p>
+<p><b>Sí para</b> los que necesitan una secuencia continua de principio y fin, para los que aman el trabajo basado en proyectos porque, satisfactorio o no, una vez finalizado pueden empezar uno nuevo.</p>
+<p><b>No para</b> aquellos que prefieren vivir de una forma cómoda y lineal.</p>
+<p><b>Cuento e imperativo:</b><br>
+Llega a tierra en un pequeño bote, vestido con una túnica gastada. Cuenta que ha sido comerciante, aunque ahora es aprendiz de carpintero. En el espacio de un año se convierte en el mayor carpintero del pueblo, rico y respetado por todos. Luego se quita la ropa cara, se pone la vieja túnica que ha guardado y se hace a la mar en la barca. Cuando llega a tierra, cuenta que ha sido carpintero, aunque ahora es sólo un aprendiz de pintor.
+</p>
+<p><b>Cambia todo: no perderás nada.</b></p>
+      `;
+  }
+  if(num === 00){//8
+    return `
+<p><b>...</b></p>
+<p>...</p>
+<p><b>Sí para</b> ...</p>
+<p><b>No para</b> ...</p>
+<p><b>Cuento e imperativo:</b><br>
+...
+</p>
+<p><b>...</b></p>
       `;
   }
 }
+
+
+
+
+
+
