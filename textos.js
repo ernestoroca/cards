2c0d223f49ac504f263d5165703777d4c686a6ea
ernestoroca <ernesto.roca@gmail.com>
2021-05-27 18:40:28 -0400

    Update textos.js

--- textos.js
+++ textos.js
@@ -1,3 +1,37 @@
 function getTexto(num){
-  return "";
+  if(num === 61){//1
+    return `
+<p><b>El cetro de los elementos</b></p>
+<p>Es la llama que aún no se ha encendido, la luz de la mañana que trae en sí todas las esperanzas de un día que no ha pasado del todo, en el que todo puede suceder. Es el comienzo, el poder antes de la acción, el mago jugando con los elementos, el niño trayendo a sus ojos los sueños de miles de existencias posibles. En el cetro están todas las premisas para una vida que se va a actualizar: tiene en sí el gozo de la ilusión, que fácilmente puede convertirse en ilusión de gozo.</p>
+<p>Sí para los que aman el momento del comienzo, el que precede a la acción, cuando nada es concreto y reina el entusiasmo por la idea.</p>
+<p>No para los que se preocupan por las dificultades de un camino que no han seguido y temen carecer de potencialidades para superarlas.</p>
+<p><b>Cuento e imperativo:</b><br>
+Le dice a alguien que se convertirá en el mejor piloto del mundo; le dice a alguien más que será el rey de los piratas; a otros les dice que se convertirá en un genio de la música. Está satisfecho con sus propósitos cambiantes. Porque todas son ciertas y nadie puede contradecirlo. Porque es un niño, con los sueños de mil existencias posibles aún en sus ojos.
+</p>
+<p><b>Vuelve al principio</b></p>
+      `;
+  }
+  if(num === 63){//8
+    return `
+<p><b></b></p>
+<p>Ícaro se eleva al cielo, sobre sus alas de plumas y cera vuela hacia el cálido sol, que derrite su arnés y lo hace caer al suelo. Hay una regla de causa y efecto según la cual cada uno recibe lo que se merece, y todo lo que sube tarde o temprano debe bajar, lo que aumenta debe disminuir al final, lo que vive está destinado a morir. Siempre hay columnas y anillos por los que tenemos que pasar: una lógica consecuencial que traza una línea entre lo que viene primero y lo que sigue. Y en esta cuerda, como equilibrista, el presente encuentra su equilibrio.</p>
+<p>Sí para aquellos que saben dar lo mejor de sí mismos cuando son conscientes de estar en un carril ya definido y conocen de antemano la evolución de aquello a lo que se dedican.</p>
+<p>No para aquellos que son torturados por tal sentimiento y quisieran rebelarse</p>
+<p><b>Cuento e imperativo:</b><br>
+Extiende sus alas hechas de plumas y pegamento y se eleva en el cielo. Se eleva, trepa nubes y corrientes de aire, hasta que se aleja del sol a una pulgada. Allí, el pegamento se disuelve, las alas se desprenden. Pero sonríe con calma porque sabe que ha llegado a la cima. Y todo lo que sube, tarde o temprano, debe bajar.
+</p>
+<p><b>El correcto es siempre el más simple</b></p>
+      `;
+  }
+  if(num === 63){//8
+    return `
+<p><b>La caída</b></p>
+<p></p>
+<p>Sí para </p>
+<p>No para </p>
+<p><b>Cuento e imperativo:</b><br>
+</p>
+<p><b></b></p>
+      `;
+  }
 }
